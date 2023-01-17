d7fac1fc8f181fb90d74af86a8c9238ebb8aba32
Vladimir Popov <wladdima.wladimir@gmail.com>
2023-01-17 20:51:48 +0100

    assert added

--- test_many_languages_task/conftest.py
+++ test_many_languages_task/conftest.py
@@ -13,21 +13,10 @@ def pytest_addoption(parser):
 @pytest.fixture(scope='function')
 def browser(request):
     browser_language = request.config.getoption('language')
-    browser = None
-    if browser_language == 'ru':
-        print('\nstart browser in russian..')
-        options_languages.add_experimental_option('prefs', {'intl.accept_languages': browser_language})
-        browser = webdriver.Chrome(options=options_languages)
-    elif browser_language == 'en':
-        print('\nstart browser in english..')
-        options_languages.add_experimental_option('prefs', {'intl.accept_languages': browser_language})
-        browser = webdriver.Chrome(options=options_languages)
-    elif browser_language == 'de':
-        print('\nstart browser in german..')
-        options_languages.add_experimental_option('prefs', {'intl.accept_languages': browser_language})
-        browser = webdriver.Chrome(options=options_languages)
-    else:
-        raise pytest.UsageError('--language should be: russian, english or german')
+
+    options_languages.add_experimental_option('prefs', {'intl.accept_languages': browser_language})
+    browser = webdriver.Chrome(options=options_languages)
+
     yield browser
     print('\nquit browser..')
     browser.quit()
--- test_many_languages_task/test_items.py
+++ test_many_languages_task/test_items.py
@@ -6,5 +6,5 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 def test_guest_should_see_add_to_basket_button(browser):
     browser.get(link)
-    browser.find_element(By.CSS_SELECTOR, '.btn-add-to-basket')
-    time.sleep(3)
+    assert browser.find_elements(By.CSS_SELECTOR, '.btn-add-to-basket'), 'There is no button "Add to basket"'
+    time.sleep(30)
